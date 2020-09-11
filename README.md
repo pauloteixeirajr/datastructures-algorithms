43c8180f75080cb799a5156f2082fc3a9c4f4eda
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-09-11 16:38:13 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Javascript Algorithms and Data Structures
+
+The functions and materials found in this repo are part of the Udemy Course Javascript Algorithms and Data Structures Masterclass Course by Colt Steele.
---
+++ bubble-sort.js
@@ -0,0 +1,22 @@
+/**
+ * Bubble Sort implementation
+ * @param {number[]} arr
+ * @returns {number[]} Sorted array
+ */
+function bubbleSort(arr) {
+  let noSwaps;
+
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // Swap index
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return arr;
+}
