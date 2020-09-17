c516afc7fc6dd9486f410b189c66502311058e65
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-09-17 19:20:26 +0100

    graph traversal

--- data-structure/graphs.js
+++ data-structure/graphs.js
@@ -45,4 +45,69 @@ class Graph {
       (vertex) => vertex !== vertex1
     );
   }
+
+  // Depth First Graph Traversal Recursive
+  DFSRecursive(start) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    function dfs(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach((neighbour) => {
+        if (!visited[neighbour]) {
+          return dfs(neighbour);
+        }
+      });
+    }
+
+    dfs(start);
+    return result;
+  }
+
+  // Depth First Graph Traversal Iterative
+  DFSIterative(start) {
+    const stack = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+    visited[start] = true;
+
+    while (stack.length) {
+      currentVertex = stack.pop();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((neighbour) => {
+        if (!visited[neighbour]) {
+          visited[neighbour] = true;
+          stack.push(neighbour);
+        }
+      });
+    }
+    return result;
+  }
+
+  // Breadth First Graph Traversal
+  BFS(start) {
+    const queue = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+    visited[start] = true;
+
+    while (queue.length) {
+      currentVertex = queue.shift();
+      result.push(currentVertex);
+      this.adjacencyList[currentVertex].forEach((neighbour) => {
+        if (!visited[neighbour]) {
+          visited[neighbour] = true;
+          queue.push(neighbour);
+        }
+      });
+    }
+
+    return result;
+  }
 }
